9c8908ab91626eb376f464e3ddb9a6c543151a3d
A_Niki3 <yuukanmuri1358@yahoo.co.jp>
2024-09-30 20:32:04 +0900

    add new event

--- src/main/java/org/niki3/ddi/Ddi.java
+++ src/main/java/org/niki3/ddi/Ddi.java
@@ -1,29 +1,20 @@
 package org.niki3.ddi;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import org.niki3.ddi.items.event.cutter_shard;
 import org.slf4j.Logger;
 import org.niki3.ddi.creative.creative_add;
 
@@ -43,6 +34,7 @@ public class Ddi {
 
         creative_add.ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(cutter_shard.class);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
