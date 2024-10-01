7e85dc38a6a820a65527d44d3e66c7d93b731692
A_Niki3 <yuukanmuri1358@yahoo.co.jp>
2024-10-01 23:08:29 +0900

    add new textures and change cutter's feature

--- src/main/java/org/niki3/ddi/Ddi.java
+++ src/main/java/org/niki3/ddi/Ddi.java
@@ -14,7 +14,6 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.niki3.ddi.items.event.cutter_shard;
 import org.slf4j.Logger;
 import org.niki3.ddi.creative.creative_add;
 
@@ -34,7 +33,7 @@ public class Ddi {
 
         creative_add.ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(cutter_shard.class);
+        //MinecraftForge.EVENT_BUS.register(cutter_shard.class);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
