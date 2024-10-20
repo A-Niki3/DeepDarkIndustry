1fc32bf6e13e292dd68fd7d560acca876bff0449
A_Niki3 <yuukanmuri1358@yahoo.co.jp>
2024-10-20 17:38:31 +0900

    fixed wool boots

--- src/main/java/org/niki3/ddi/mixin/vib_mixin.java
+++ src/main/java/org/niki3/ddi/mixin/vib_mixin.java
@@ -14,26 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(VibrationListener.class)
 public class vib_mixin {
-    /*
-    @Inject(method = "handleGameEvent",at = @At("RETURN"))
-    private void onHandleGameEvent(ServerLevel level, GameEvent.Message message, CallbackInfoReturnable<Boolean> cir){
-        System.out.println("GameEvent:" + message.gameEvent().getName());
-        //スカルクセンサーのイベント：sculk_sensor_tendrils_clicking
-        //スカルクシュリーカーのイベント：shriek
-    }
-    */
-    @Inject(method = "handleGameEvent",at = @At("HEAD"), cancellable = true)
-    private void sculk_cancel(ServerLevel level, GameEvent.Message message, CallbackInfoReturnable<Boolean> cir){
-        if(message.gameEvent().getName().equals("sculk_sensor_tendrils_clicking") || message.gameEvent().getName().equals("shriek")){
-            //System.out.println("sculks event");
+    @Inject(method = "handleGameEvent",at = @At("HEAD"),cancellable = true)
+    private void Vibration_Canceler(ServerLevel level, GameEvent.Message message, CallbackInfoReturnable<Boolean> cir) {
+        if(message.gameEvent().getName().equals("step") || message.gameEvent().getName().equals("hit_ground")){
             Entity entity = message.context().sourceEntity();
-
-            if (entity instanceof ServerPlayer player){
-                //System.out.println("check player");
+            if(entity instanceof ServerPlayer player){
                 ItemStack boots = player.getInventory().getArmor(0);
-
                 if(boots.is(add_tags.VIBRATION_SILENCER)){
-                    //System.out.println("event cancel");
                     cir.setReturnValue(false);
                 }
             }
