8a5de71197a70fe11034e6f2273b6b1f83962a00
niki3 <yuukanmuri@gmail.com>
2024-10-23 16:35:01 +0900

    Preparing to add new blocks

---
+++ src/main/java/org/niki3/ddi/blocks/addBlock.java
@@ -0,0 +1,10 @@
+package org.niki3.ddi.blocks;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.niki3.ddi.Ddi;
+
+public class addBlock {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Ddi.MODID);
+}
